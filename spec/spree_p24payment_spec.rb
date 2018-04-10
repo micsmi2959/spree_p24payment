0229eeac747d6e17c756b544764725aa3b1df7e4
micsmi2959 <mic.smiechowicz@gmail.com>
2018-04-10 13:56:12 +0200

    next

--- spec/spree_p24payment_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe SpreeP24payment do
-  it "has a version number" do
-    expect(SpreeP24payment::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
